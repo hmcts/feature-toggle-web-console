3af9f398cb0643a603f31e83de17bffeafcd1159
nitinprabhu <prabhunitin61@gmail.com>
2018-05-14 21:07:11 +0100

    Removed mutliple servlet config

--- src/main/java/uk/gov/hmcts/reform/featuretoggle/config/FF4jServletConfig.java
+++ src/main/java/uk/gov/hmcts/reform/featuretoggle/config/FF4jServletConfig.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.featuretoggle.config;
 
-import org.ff4j.FF4j;
 import org.ff4j.web.FF4jDispatcherServlet;
 import org.ff4j.web.embedded.ConsoleServlet;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -21,13 +19,5 @@ public class FF4jServletConfig extends SpringBootServletInitializer {
     @Bean
     public ServletRegistrationBean ff4jDispatcherServletRegistrationBean(FF4jDispatcherServlet ff4jDispatcherServlet) {
         return new ServletRegistrationBean(ff4jDispatcherServlet, "/ff4j-web-console/*");
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public FF4jDispatcherServlet getFF4jDispatcherServlet(FF4j ff4j) {
-        FF4jDispatcherServlet ff4jConsoleServlet = new FF4jDispatcherServlet();
-        ff4jConsoleServlet.setFf4j(ff4j);
-        return ff4jConsoleServlet;
     }
 }
